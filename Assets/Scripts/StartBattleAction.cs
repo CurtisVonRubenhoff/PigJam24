5c831ae0096418e1434dfcab20eb55d7e6b4fa2d
Curtis Cummings <curtis.cummings-pd@status.pro>
2024-10-13 18:46:31 -0500

    make battle work

---
+++ Assets/Scripts/StartBattleAction.cs
@@ -0,0 +1,11 @@
+﻿using CleverCrow.Fluid.Dialogues.Actions;
+using CleverCrow.Fluid.Dialogues;
+
+[CreateMenu("Custom/StartBattle")]
+public class CustomAction : ActionDataBase {
+
+    public override void OnStart () {
+        Battle.StartBattle();
+    }
+
+}
