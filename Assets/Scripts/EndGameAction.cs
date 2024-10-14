ba8007f093ce6c2ecc8b22f1131baf7ca890cab6
Curtis Cummings <curtis.cummings-pd@status.pro>
2024-10-13 19:45:22 -0500

    add credits scene

---
+++ Assets/Scripts/EndGameAction.cs
@@ -0,0 +1,14 @@
+﻿using CleverCrow.Fluid.Dialogues.Actions;
+using CleverCrow.Fluid.Dialogues;
+using UnityEngine.WSA;
+using UnityEngine;
+
+[CreateMenu("Custom/EndGame")]
+public class EndGameAction : ActionDataBase {
+
+    public override void OnStart ()
+    {
+        UnityEngine.Application.Quit();
+    }
+
+}
